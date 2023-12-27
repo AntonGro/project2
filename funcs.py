42016b5e296c0dad915ace24f3cc651eec0c3791
Anton Gromov <antongromov1234@yandex.ru>
2023-12-27 10:10:21 +0300

    3st commit

--- funcs.py
+++ funcs.py
@@ -1,5 +1,6 @@
 def addition(a, b):
     print(f"Result: {a+b}")
 
+
 def substraction(a, b):
     print(f"Result: {a-b}")
---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import addition
+from funcs import substraction
+
+a = int(input("1st nunmber: "))
+b = int(input("2nd number: "))
+addition(a, b)
+substraction(a, b)
